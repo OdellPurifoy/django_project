c1737c3120b09a3443c797266db3a1225106e25f
Odell <odell.purifoy@gmail.com>
2020-06-09 20:15:18 -0400

    Added base template and bootstrap

--- django_project/settings.py
+++ django_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'more_alike_mvp.apps.MoreAlikeMvpConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
