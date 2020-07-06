b9acc7e8143ea8eb56ad227d37dd21302a04909d
Odell <odell.purifoy@gmail.com>
2020-07-06 18:03:55 -0400

    Base CRUD Comment flow

--- more_alike_mvp/urls.py
+++ more_alike_mvp/urls.py
@@ -9,5 +9,8 @@ urlpatterns = [
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
+    path('comment/<int:pk>/approve/', views.comment_approve, name='comment_approve'),
+    path('comment/<int:pk>/remove/', views.comment_remove, name='comment_remove'),
     path('about/', views.about, name='more-alike-about'),
 ]
