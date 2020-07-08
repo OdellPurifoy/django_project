c9304903b0719c570a060ae086686a310bf44985
Odell <odell.purifoy@gmail.com>
2020-07-08 19:27:18 -0400

    Styling changes and post category field added

--- more_alike_mvp/models.py
+++ more_alike_mvp/models.py
@@ -3,11 +3,22 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
 
+
 class Post(models.Model):
+    # This will render in the 'category' post form field.
+    POST_CATEGORIES = [
+        ('none', 'None'),
+        ('racial equality', 'Racial Equality'),
+        ('police reform', 'Police Reform'),
+        ('gender equality', 'Gender Equality'),
+        ('relogion', 'Religion')
+    ]
+
     title = models.CharField(max_length=100)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.CharField(max_length=100, choices=POST_CATEGORIES, default='None')
 
     def __str__(self):
         return self.title
